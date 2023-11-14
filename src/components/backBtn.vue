1459690f9d5151323720f2c6ec4b148ca22a1355
xiaoyangii <90941964+xiaoyangii@users.noreply.github.com>
2023-11-14 23:27:32 +0800

    feat: 封装返回按钮组件和好友列表

---
+++ src/components/backBtn.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button class="back" @click="$router.push('/home')">返回主界面</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'back',
+  data () {
+    return {}
+  },
+  computed: {},
+  methods: {},
+  created () {},
+}
+</script>
+<style scoped lang='less'>
+.back {
+  margin-top: 5vh;
+  height: 10vh;
+  width: 15vw;
+  .pxfont(40);
+  border-radius: 1vw;
+  background-color: rgb(28, 65, 187);
+  &:hover {
+    -webkit-box-shadow: 0.5vh 1vw 2.1vh 0.6vw rgba(76,201,240,1);
+    -moz-box-shadow: 0.5vh 1vw 2.1vh 0.6vw rgba(76,201,240,1);
+    box-shadow: 0.5vh 0.5vw 8.1vh 0.5vw rgba(76,201,240,1);
+    transition: all 0.25s linear;
+    transform: scale(1.05);
+  }
+}
+</style>
---
+++ src/components/fri.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="f" :style="customStyle">
+    <div class="f_head">好友列表</div>
+    <div class="f_body">
+      <div class="online">
+        <div class="online_title">在线</div>
+        <div class="online_list"></div>
+      </div>
+      <div class="offline">
+        <div class="offline_title">离线</div>
+        <div class="offline_list"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'f',
+  data () {
+    return {}
+  },
+  props: {
+    list: {
+      type: Array,
+      default: () => []
+    },
+    customStyle: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created () {},
+}
+</script>
+<style scoped lang='less'>
+.f {
+  width: 20vw;
+  height: 60vh;
+  background-color: #fff;
+  opacity: 0.6;
+  border: 0.2vw solid black;
+  border-right-width: 0.25vw;
+  border-radius: 1vw;
+  &_head {
+    width: 100%;
+    height: 8vh;
+    line-height: 8vh;
+    text-align: center;
+    .pxfont(40)
+  }
+  &_body {
+    width: 100%;
+    height: calc(100% - 8vh);
+  }
+}
+.online, .offline {
+  width: 100%;
+  height: 50%;
+}
+</style>
