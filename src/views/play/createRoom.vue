2a13934ad23de2f22b4c0b3011b86e403771b2f9
xiaoyangii <90941964+xiaoyangii@users.noreply.github.com>
2023-11-15 22:10:09 +0800

    feat: add createRoom page :lipstick:

--- src/views/play/createRoom.vue
+++ src/views/play/createRoom.vue
@@ -1,15 +1,70 @@
 <template>
   <div class="room">
-    room
+    <div class="room_logo">
+      <img src="@/assets/images/logo.png">
+    </div>
+    <div class="room_body">
+      <div class="room_body_creation">
+        <div class="creation_title">创建房间</div>
+        <div class="creation_invition">
+          <div class="creation_invition_room">
+            <div class="creation_invition_room_text">房间邀请码</div>
+            <div class="creation_invition_room_code">{{ code }}</div>
+          </div>
+          <div class="creation_invition_code">
+            <div class="creation_invition_room_text">输入邀请码</div>
+            <input type="text" class="creation_invition_code_ipt" placeholder="请输入邀请码">
+          </div>
+        </div>
+        <div class="creation_player">
+          <div class="creation_player_text">当前玩家</div>
+          <div class="creation_player_master">
+            <div class="creation_player_status" :style="master.status?ready:noready"></div>
+            <div class="creation_player_name">{{ master.name }}(我)</div>
+          </div>
+          <div class="creation_player_other">
+            <div class="creation_player_status" :style="player.status?ready:noready"></div>
+            <div class="creation_player_name">{{ '等待中...'||player.name }}</div>
+          </div>
+        </div>
+        <div class="creation_btnbox">
+          <button class="begin">开始</button>
+          <button class="return" @click="$router.push('/home')">返回</button>
+        </div>
+      </div>
+      <Fri :style="customStyle"></Fri>
+    </div>
   </div>
 </template>
 
 <script>
+import Fri from '@/components/fri.vue'
 export default {
   name: 'createRoom',
-  components: {},
+  components: {
+    Fri
+  },
   data () {
-    return {}
+    return {
+      customStyle: {
+        height: '58vh'
+      },
+      ready: {
+        backgroundColor: 'green'
+      },
+      noready: {
+        backgroundColor: 'red'
+      },
+      code: 'vivo50',
+      master: {
+        name: 'xh懒觉大支',
+        status: true
+      },
+      player: {
+        name: 'xh懒觉小支',
+        status: false
+      }
+    }
   },
   computed: {},
   watch: {},
@@ -18,5 +73,134 @@ export default {
 }
 </script>
 <style scoped lang='less'>
-
+.room {
+  height: 100vh;
+  width: 100vw;
+  &_logo {
+    width: 100vw;
+    height: 40vh;
+    text-align: center;
+    img {
+      height: 100%;
+      scale: 1;
+    }
+  }
+}
+.room_body {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 60vh;
+  // padding: 0 12vw;
+  &_creation {
+    width: 50%;
+    height: 58vh;
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 1vw;
+    flex-shrink: 0;
+    margin-right: 1vw;
+  }
+}
+.creation {
+  &_title {
+    height: 8vh;
+    line-height: 8vh;
+    width: 100%;
+    text-align: center;
+    .pxfont(40)
+  }
+  &_invition {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 20vh;
+    width: 100%;
+    &_room, &_code {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      height: 50%;
+      width: 100%;
+      &_text {
+        .pxfont(40);
+        font-weight: 550;
+        margin-right: 2vw;
+      }
+      &_code, &_ipt {
+        width: 50%;
+        height: 7vh;
+        line-height: 6vh;
+        background-color: #fff;
+        text-align: center;
+        border: 0.2vw solid black;
+        .pxfont(36);
+      }
+    }
+  }
+  &_player {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 20vh;
+    width: 100%;
+    &_text {
+      .pxfont(40);
+      font-weight: 550;
+      margin-right: 2.7vw;
+    }
+    &_master, &_other {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: 12vh;
+      width: 12vw;
+      background-color: #fff;
+      border: #dfd9d9 solid 0.1vw;
+    }
+    &_master {
+      margin-right: 2vw;
+    }
+    &_status {
+      width: 100%;
+      height: 1vh;
+    }
+    &_name {
+      width: 100%;
+      height: calc(100%-1vh);
+      line-height: 11vh;
+      text-align: center;
+      .pxfont(23);
+    }
+  }
+  &_btnbox {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 10vh;
+    width: 100%;
+    .begin, .return {
+      height: 70%;
+      width: 20%;
+      background-color: #629DF4;
+      border-radius: 1vw;
+      .pxfont(32);
+      font-weight: 550;
+      letter-spacing: 0.2vw;
+      text-align: center;
+      &:hover {
+        transition: all 0.2s linear;
+        transform: scale(1.05);
+      }
+    }
+    .begin {
+      margin-right: 8vw;
+    }
+  }
+}
 </style>
--- src/views/play/settlement.vue
+++ src/views/play/settlement.vue
@@ -61,7 +61,7 @@ export default {
     margin: 0 auto;
     width: 80%;
     height: 70vh;
-    background-color: rgb(28, 65, 187);
+    background-color: rgba(28, 65, 187, 0.4);
     border-radius: 2vw;
     &_title {
       height: 10vh;
@@ -73,21 +73,6 @@ export default {
     &_table {
       width: 100%;
       height: 60vh;
-    }
-  }
-  &_back {
-    margin-top: 5vh;
-    height: 10vh;
-    width: 15vw;
-    .pxfont(40);
-    border-radius: 1vw;
-    background-color: rgb(28, 65, 187);
-    &:hover {
-      -webkit-box-shadow: 0.5vh 1vw 2.1vh 0.6vw rgba(76,201,240,1);
-      -moz-box-shadow: 0.5vh 1vw 2.1vh 0.6vw rgba(76,201,240,1);
-      box-shadow: 0.5vh 0.5vw 8.1vh 0.5vw rgba(76,201,240,1);
-      transition: all 0.25s linear;
-      transform: scale(1.05);
     }
   }
 }
@@ -102,6 +87,7 @@ th, td {
   border: 0.1vw solid #000;
   padding: 0.1vw;
   background-color: #699eca;
+  opacity: 0.8;
   .pxfont(36);
   text-align: center;
 }
