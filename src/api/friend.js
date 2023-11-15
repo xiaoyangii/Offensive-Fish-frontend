612c4426fa7e25e6d5d875dc3eccbeccd9068977
xiaoyangii <90941964+xiaoyangii@users.noreply.github.com>
2023-11-15 22:05:18 +0800

    feat: packaged friend rank api :art:

---
+++ src/api/friend.js
@@ -0,0 +1,30 @@
+import request from '@/utils/request.js'
+
+// 1.获取好友列表接口
+export const getFriendList = (userName) => {
+  return request.get('/FriendLink/selectAllFriend', {
+    params: {
+      userName
+    }
+  })
+}
+
+// 2.添加好友接口
+export const addFriend = (userName, friendName) => {
+  return request.get('/FriendLink/addFriendLink', {
+    params: {
+      userName,
+      friendName
+    }
+  })
+}
+
+// 3.删除好友接口
+export const deleteFriend = (userName, friendName) => {
+  return request.get('/FriendLink/deleteFriendLink', {
+    params: {
+      userName,
+      friendName
+    }
+  })
+}
--- src/api/rank.js
+++ src/api/rank.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 
-//1.获取排行榜
+// 1.获取排行榜
 export const getRankList = () => {
-  return request.get('/Rank/getRankList')
+  return request.get('/RankList/selectAllRankList')
 }
