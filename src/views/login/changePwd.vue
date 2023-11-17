098d75c25fb5faa3f54201096e33a4b6ccc39235
xiaoyangii <90941964+xiaoyangii@users.noreply.github.com>
2023-11-17 14:17:58 +0800

    feat: 更新修改密码代码结构 :art:

--- src/views/login/changePwd.vue
+++ src/views/login/changePwd.vue
@@ -18,12 +18,12 @@
           </div>
           <div class="cgpwd_form_container">
             <label for="repassword" class="cgpwd_form_container_pwdlb">再次确认:</label>
-            <input type="password" id="repassword" class="cgpwd_form_container_pwd re" v-model="passwd" placeholder="请再次输入密码">
+            <input type="password" id="repassword" class="cgpwd_form_container_pwd re" v-model="repasswd" placeholder="请再次输入密码">
           </div>
           <div class="cgpwd_form_container">
             <label for="code" class="cgpwd_form_container_codelb">验证码:</label>
             <input type="tel" id="code" class="cgpwd_form_container_code" v-model="validate" placeholder="请输入6位验证码">
-            <button @click="getCode" class="sendmsg">
+            <button @click="getCode()" class="sendmsg">
               {{ second === totalSecond ? '获取验证码' : second + '秒后重新发送'}}
             </button>
           </div>
@@ -38,7 +38,8 @@
 </template>
 
 <script>
-import { codeForgetPassword } from '@/api/login.js'
+import { codeForgetPassword, getMsgCode } from '@/api/login.js'
+import { validChg, validTel } from '@/utils/validate.js'
 export default {
   name: 'changePwd',
   components: {},
@@ -46,8 +47,9 @@ export default {
     return {
       passwd: '',
       userName: '',
-      name: '',
+      repasswd: '',
       validate: '',
+      verifyCode: '',
       totalSecond: 60, // 总秒数
       second: 60, // 当前秒数，开定时器对 second--
       timer: null, // 定时器 id
@@ -55,7 +57,46 @@ export default {
   },
   methods: {
     async changePwd() {
-      await codeForgetPassword(this.userName, this.passwd, this.validate)
+      if (!validChg(this.userName, this.passwd, this.repasswd, this.validate)) return
+      await codeForgetPassword(this.userName, this.validate, this.passwd, this.verifyCode)
+      .then(res => {
+        if(res.data.msg == '验证码输入错误!') {
+          this.$message.error('验证码输入错误')
+        } else if(res.data.msg == '账号输入错误!') {
+          this.$message.error('账号输入错误!')
+        } else {
+          this.$message.success('修改成功')
+          this.$router.push('/login')
+        }
+      })
+      .catch(err => {
+        this.$message.error('修改失败')
+      })
+    },
+    async getCode() {
+      if (!validTel(this.userName)) {
+        return
+      }
+      // 当前目前没有定时器开着，且 totalSecond 和 second 一致 (秒数归位) 才可以倒计时
+      if (!this.timer && this.second === this.totalSecond) {
+        await getMsgCode(this.userName)
+        .then(res => {
+          this.$message.success('验证码已发送')
+          // 开启倒计时
+          this.verifyCode = res.headers['content-disposition'].split(';')[1].split('=')[1]
+          this.timer = setInterval(() => {
+            this.second--
+            if (this.second <= 0) {
+              clearInterval(this.timer)
+              this.timer = null // 重置定时器 id
+              this.second = this.totalSecond // 归位
+            }
+          }, 1000)
+        })
+        .catch(err => {
+          this.$message.error('验证码发送失败')
+        })
+      }
     }
   }
 }
