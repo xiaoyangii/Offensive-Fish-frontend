622d18eb9dea3ec6a2523337be35b70f9402cfc4
xiaoyangii <90941964+xiaoyangii@users.noreply.github.com>
2023-11-17 14:18:44 +0800

    feat: 新增角色详情介绍页面 :sparkles:

--- src/views/layout/character.vue
+++ src/views/layout/character.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="character">
-    role
+    <div class="character_left">
+      <div class="character_left_card" v-for="role in roles" :key="role.id">
+        <div class="card_wrap">
+
+        </div>
+      </div>
+    </div>
+    <div class="character_right">
+
+    </div>
   </div>
 </template>
 
 <script>
+import Roles from '@/dicts/static_roles.js'
 export default {
   name: 'character',
   components: {},
   data () {
-    return {}
+    return {
+      roles: Roles.roles
+    }
   },
   computed: {},
   watch: {},
@@ -18,5 +30,48 @@ export default {
 }
 </script>
 <style scoped lang='less'>
-/* @import url(); 引入公共css类 */
+.character {
+  display: flex;
+  flex-direction: row;
+  padding: 0 8vw;
+  width: 100vw;
+  height: 100vh;
+  &_left {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    flex: 1.2;
+    height: 100%;
+    padding: 20vh 0;
+    &_card {
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      width: 80%;
+      height: 40%;
+      background-color: #fff;
+      &:first-child {
+        border-top-left-radius: 0.8vw;
+
+      }
+      &:last-child {
+        border-bottom-left-radius: 0.8vw;
+      }
+    }
+  }
+  &_right {
+    flex: 5;
+    height: 100%;
+    background-color: blue;
+  }
+}
+.card {
+  &_wrap {
+    height: 90%;
+    width: 93%;
+    background-color: #78AAF2;
+    border-top-left-radius: 0.8vw;
+    border-bottom-left-radius: 0.8vw;
+  }
+}
 </style>
