e7d965fa6d97143b76e852d343404cc86c0ee494
xiaoyangii <90941964+xiaoyangii@users.noreply.github.com>
2023-11-24 23:50:39 +0800

    feat: remove redundancy 🎨

--- src/helper/registerElementComponents.js
+++ src/helper/registerElementComponents.js
@@ -3,11 +3,8 @@ import 'element-ui/lib/theme-chalk/index.css'
 import 'element-ui/lib/theme-chalk/base.css'
 import {
   Message,
-  MessageBox,
   Loading
 } from 'element-ui'
 
 Vue.prototype.$message = Message
-Vue.prototype.$messageBox = MessageBox
 Vue.prototype.$loading = Loading.service
-Vue.prototype.$confirm = MessageBox.confirm
