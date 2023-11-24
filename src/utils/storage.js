651d50b0ea33ef49f698b3efb64dc575c4d3190b
xiaoyangii <90941964+xiaoyangii@users.noreply.github.com>
2023-11-24 23:52:58 +0800

    feat: add localStorage room_info :sparkles:

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,5 +1,6 @@
 // 约定一个通用的键名
 const INFO_KEY = 'user_info'
+const ROOM_KEY = 'room_info'
 
 // 获取个人信息
 export const getInfo = () => {
@@ -24,4 +25,16 @@ export const setToken = (newToken) => {
 // 移除个人信息
 export const removeInfo = () => {
   localStorage.removeItem(INFO_KEY)
+}
+
+// 存储房间信息
+export const setRoomInfoByLocal = (obj) => {
+  localStorage.setItem(ROOM_KEY, JSON.stringify(obj))
+}
+
+// 获取房间信息
+export const getRoomInfoByLocal = () => {
+  const defaultObj = { roomId: '', roomOwnerId: '', code: '', numbers: 1, isOpen: 1, playerId: '' }
+  const result = localStorage.getItem(ROOM_KEY)
+  return result ? JSON.parse(result) : defaultObj
 }
