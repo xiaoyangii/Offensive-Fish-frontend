d16ced7d96697e14d8b22590351f2190cdf83cd8
xiaoyangii <90941964+xiaoyangii@users.noreply.github.com>
2023-12-10 13:52:20 +0800

    fix: fix eat and splice fish, move bugs :bug:

--- src/views/play/game.vue
+++ src/views/play/game.vue
@@ -10,14 +10,14 @@
       <div class="box_display">
         <div class="text">P1</div>
         <div class="wrap">
-          <div class="health p1"></div>
+          <div class="health p1" ref="p1"></div>
         </div>
         <div class="score">{{ scoreMaster }}</div>
       </div>
       <div class="box_display">
         <div class="text">P2</div>
         <div class="wrap">
-          <div class="health p2"></div>
+          <div class="health p2" ref="p2"></div>
         </div>
         <div class="score">{{ scorePlayer }}</div>
       </div>
@@ -100,12 +100,12 @@ export default {
   },
   watch: {
     // 监听master和player的血量，如果血量为0，游戏结束
-    'master.health': (val) => {
+    'master.health': function(val) {
       if(val <= 0) {
         this.goSettle()
       }
     },
-    'player.health': (val) => {
+    'player.health': function(val) {
       if(val <= 0) {
         this.goSettle()
       }
@@ -116,8 +116,7 @@ export default {
       this.stopAnimation()
       store.commit('socket/setScore', { role: 'master', score: this.scoreMaster })
       store.commit('socket/setScore', { role: 'player', score: this.scorePlayer })
-      // this.socket.emit('gameOver', this.roomId)
-      // this.$router.push({ path: '/settlement' })
+      this.$router.push({ path: '/settlement' })
     },
     initImage(roleId, flag) {
       const img = new Image()
@@ -144,7 +143,7 @@ export default {
       this.ctx.drawImage(this.player.img, this.player.x , this.player.y , this.w, this.h)
       this.ctx.fill()
     },
-    drawOtherFish(fishInfo) { //双缓冲绘制鱼
+    drawOtherFish(fishInfo) {
       const offscreenCanvas = document.createElement('canvas')
       offscreenCanvas.width = this.fishCanvas.width
       offscreenCanvas.height = this.fishCanvas.height
@@ -203,9 +202,14 @@ export default {
           break
       }
       // 如果没有任何方向键被按下，停止动画循环
-      if(!this.player.moving.up && !this.player.moving.down && !this.player.moving.left && !this.player.moving.right &&
-        !this.master.moving.up && !this.master.moving.down && !this.master.moving.left && !this.master.moving.right) {
-        this.stopAnimation()
+      if(this.isMaster) {
+        if(!this.master.moving.up && !this.master.moving.down && !this.master.moving.left && !this.master.moving.right) {
+          this.stopAnimation()
+        }
+      } else {
+        if(!this.player.moving.up && !this.player.moving.down && !this.player.moving.left && !this.player.moving.right) {
+          this.stopAnimation()
+        }
       }
     },
     movePlayer() {
@@ -234,35 +238,42 @@ export default {
       for (let i = 0; i < this.fishes.length; i++) {
         const fish = this.fishes[i]
         if (this.checkCollision(roler, fish)) {
-          fish.isIn = true
           console.log('玩家战斗力:', roler.defense, ' 鱼战斗力:', fish.defense)
           if(roler.defense >= fish.defense) {
-            this.eatFish(fish, i)
+            this.eatFish(fish, fish.num)
             i-- // 更新索引，数组长度已经改变
           } else {
-            if(this.isMaster) {
+            if(this.isMaster && !fish.isIn) {
+              fish.isIn = true
               this.master.health -= 10
-              // this.socket.emit('health', this.roomId, this.master.health)
-            } else {
+              this.socket.emit('health', this.roomId, this.master.health)
+              this.$refs.p1.style.width = `${this.master.health}%`
+            } else if(!this.isMaster && !fish.isIn){
+              fish.isIn = true
               this.player.health -= 10
-              // this.socket.emit('health', this.roomId, this.player.health)
+              this.socket.emit('health', this.roomId, this.player.health)
+              this.$refs.p2.style.width = `${this.player.health}%`
             }
           }
+        } else {
+          fish.isIn = false
         }
       }
       this.drawFish()
     },
     startAnimation() {
-      // 如果动画循环已经开始，直接返回
-      if (this.animationId !== null) {
-        return
-      }
-      const animate = () => {
-        this.movePlayer()
-        this.animationId = requestAnimationFrame(animate)
-      };
+      if(this.master.moving.up || this.master.moving.down || this.master.moving.left || this.master.moving.right ||
+      this.player.moving.up || this.player.moving.down || this.player.moving.left || this.player.moving.right) {
+        if (this.animationId !== null) {
+          return
+        }
+        const animate = () => {
+          this.movePlayer()
+          this.animationId = requestAnimationFrame(animate)
+        }
 
-      this.animationId = requestAnimationFrame(animate)
+        this.animationId = requestAnimationFrame(animate)
+      }
     },
     stopAnimation() {
       cancelAnimationFrame(this.animationId)
@@ -279,7 +290,11 @@ export default {
     eatFish(fish, index) {
       console.log('eat:', fish)
       this.socket.emit('eatFish', this.roomId, index)
-      clearInterval(this.fishes[index].fishTimer)
+      // 清除num==index的鱼的定时器
+      clearInterval(this.fishes.find(f => f.num === index)?.fishTimer)
+      // 将鱼的位置设定为画布外
+      fish.x = -100
+      fish.y = -100
       this.fishCtx.clearRect(0, 0, this.fishCanvas.width, this.fishCanvas.height)
       if(this.isMaster) {
         this.scoreMaster += 1
@@ -288,7 +303,10 @@ export default {
         this.scorePlayer += 1
         store.commit('socket/setScore', { role: 'player', score: this.scorePlayer })
       }
-      this.fishes.splice(index, 1)
+      const fishIndex = this.fishes.findIndex(f => f.num === index)
+      if (fishIndex !== -1) {
+        this.fishes.splice(fishIndex, 1)
+      }
     },
   },
   mounted() {
@@ -330,7 +348,7 @@ export default {
     this.socket.on('fish', (data) => {
       // 处理从服务器广播的鱼的生成信息
       console.log(data)
-      let { lr, SerialNum, y } = data
+      let { lr, SerialNum, y, num } = data
       let x = lr === 0 ? -35 : this.canvas.width + 35 // 从左或右边界出现
       let speed = 0.3
       if(y > 0.86) {
@@ -339,7 +357,7 @@ export default {
       // fish.img.src = `path/to/fish${SerialNum}.png` // 替换为你实际的鱼的图片路径
       // defense 设置1-10的随机数
       let defense = Math.floor(Math.random() * 10) + 1
-      let fish = { x, y: y*this.canvas.height, speed, SerialNum, lr, img: new Image(), fishTimer: null, isIn: false, defense }
+      let fish = { x, y: y*this.canvas.height, speed, SerialNum, lr, img: new Image(), fishTimer: null, isIn: false, defense, num }
       if(lr === 0) {
         fish.img.src = fish1
       } else {
@@ -350,7 +368,6 @@ export default {
         fish.x += fish.speed * (lr === 0 ? 1 : -1)
         if(fish.x < -35 || fish.x > this.canvas.width + 35) {
           clearInterval(fish.fishTimer)
-          this.fishes.splice(this.fishes.indexOf(fish), 1)
         }
         this.drawOtherFish(fish)
         // 如果鱼超出边界，可以在这里处理
@@ -362,7 +379,10 @@ export default {
     this.socket.on('anotherEat', (userId, index) => {
       // 处理从服务器广播的鱼的消失信息
       if(userId !== this.loginId) {
-        this.fishes.splice(index, 1)
+        const fishIndex = this.fishes.findIndex(f => f.num === index)
+        if (fishIndex !== -1) {
+          this.fishes.splice(fishIndex, 1)
+        }
         if(this.isMaster) {
           this.scorePlayer += 1
           store.commit('socket/setScore', { role: 'player', score: this.scorePlayer })
@@ -378,18 +398,20 @@ export default {
       if(userId !== this.loginId) {
         if(this.isMaster) {
           this.player.health = health
+          this.$refs.p2.style.width = `${this.player.health}%`
         } else {
           this.master.health = health
+          this.$refs.p1.style.width = `${this.master.health}%`
         }
       }
     })
     // 设置定时器，在 created 时触发，每隔 2s 到 4s 向 socket 发起 emit 事件
     this.randomInterval = Math.floor(Math.random() * (6000 - 4000 + 1) + 4000) // 随机间隔
     if(this.isMaster) {
-      // this.fishTimer = setInterval(() => {
-        // console.log(this.randomInterval)
+      this.fishTimer = setInterval(() => {
+        console.log(this.randomInterval)
         this.socket.emit('fishSpace', this.roomId)
-      // }, this.randomInterval)
+      }, this.randomInterval)
     }
     store.commit('socket/setSocket', this.socket)
   },
@@ -398,6 +420,7 @@ export default {
     for (const fish of this.fishes) {
       clearInterval(fish.fishTimer)
     }
+    clearInterval(this.fishTimer)
     // 清空画布
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
     this.fishCtx.clearRect(0, 0, this.fishCanvas.width, this.fishCanvas.height)
